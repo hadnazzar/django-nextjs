19636720eb1167e84da723313bbf51c5d1f2d231
hadnazzar <melih193@gmail.com>
2023-08-17 00:01:36 +0300

    feat(): add selected field to products

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,6 +5,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
+    selected = models.BooleanField(default=False)  # Added this line
+
 
     def __str__(self):
         return self.name
